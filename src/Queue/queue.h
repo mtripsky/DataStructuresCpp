1056e437d9edeb79057fdc71e9952f9d64f0addc
mates <m.tripsky@gmail.com>
2019-06-02 12:34:29 +0200

    Formatting of src codes

--- src/Queue/queue.h
+++ src/Queue/queue.h
@@ -7,120 +7,118 @@
 namespace dsc {
 namespace fifo {
 
-template <typename T>
+template<typename T>
 class Queue {
 public:
-    // -------------------------------------------------------------------------
-    // CONSTRUCTORS
-    // -------------------------------------------------------------------------
-    Queue(std::size_t maxSize) noexcept : m_maxSize(maxSize), m_size(0), m_front(0), m_rear(0)
-    {
-        m_queue = new T[maxSize + 1];
-    }
+	// -------------------------------------------------------------------------
+	// CONSTRUCTORS
+	// -------------------------------------------------------------------------
+	Queue(std::size_t maxSize) noexcept
+		: m_maxSize(maxSize)
+		, m_size(0)
+		, m_front(0)
+		, m_rear(0)
+	{
+		m_queue = new T[maxSize + 1];
+	}
 
-    // -------------------------------------------------------------------------
-    // DESTRUCTORS
-    // -------------------------------------------------------------------------
-    ~Queue()
-    {
-        delete[] m_queue;
-    }
+	// -------------------------------------------------------------------------
+	// DESTRUCTORS
+	// -------------------------------------------------------------------------
+	~Queue()
+	{
+		delete[] m_queue;
+	}
 
-    // -------------------------------------------------------------------------
-    // METHODS
-    // -------------------------------------------------------------------------
-    bool enqueue(const T& elem)
-    {
-        if(isFull())
-            return false;
+	// -------------------------------------------------------------------------
+	// METHODS
+	// -------------------------------------------------------------------------
+	bool enqueue(const T& elem)
+	{
+		if(isFull())
+			return false;
 
-        if(m_rear + 1 > m_maxSize)
-        {
-            m_rear = 0;
-            m_queue[m_rear] = elem;
-        }
-        else
-        {
-            m_queue[m_rear] = elem;
-        }
-        ++m_rear;
-        ++updateSize();
+		if(m_rear + 1 > m_maxSize) {
+			m_rear = 0;
+			m_queue[m_rear] = elem;
+		}
+		else {
+			m_queue[m_rear] = elem;
+		}
+		++m_rear;
+		++updateSize();
 
-        return true;
-    }
+		return true;
+	}
 
-    bool dequeue()
-    {
-        if(isEmpty())
-        {
-            return false;
-        }
-        --updateSize();
-        m_front = m_front + 1 > m_maxSize - 1 ? 0 : ++m_front;
+	bool dequeue()
+	{
+		if(isEmpty()) {
+			return false;
+		}
+		--updateSize();
+		m_front = m_front + 1 > m_maxSize - 1 ? 0 : ++m_front;
 
-        return true;
-    }
+		return true;
+	}
 
-    const T& front() const
-    {
-        if(isEmpty())
-        {
-            throw "Queue is empty";
-        }
+	const T& front() const
+	{
+		if(isEmpty()) {
+			throw "Queue is empty";
+		}
 
-        return m_queue[m_front];
-    }
+		return m_queue[m_front];
+	}
 
-    const bool isEmpty() const
-    {
-        return size() == 0;
-    }
+	const bool isEmpty() const
+	{
+		return size() == 0;
+	}
 
-    bool isFull()
-    {
-        return size() == m_maxSize;
-    }
+	bool isFull()
+	{
+		return size() == m_maxSize;
+	}
 
-    const size_t size() const
-    {
-        return m_size;
-    }
+	const size_t size() const
+	{
+		return m_size;
+	}
 
-    void print()
-    {
-        if(isEmpty())
-            std::cout << "Queue is empty\n";
-        else
-        {
-            auto index = m_front;
+	void print()
+	{
+		if(isEmpty())
+			std::cout << "Queue is empty\n";
+		else {
+			auto index = m_front;
 
-            for(auto i = 0; i < size(); ++i)
-            {
-                index = index + 1 > m_maxSize ? 0 : index;
-                std::cout << m_queue[index] << "->";
-                ++index;
-            }
-            std::cout << std::endl;
-        }
-    }
+			for(auto i = 0; i < size(); ++i) {
+				index = index + 1 > m_maxSize ? 0 : index;
+				std::cout << m_queue[index] << "->";
+				++index;
+			}
+			std::cout << std::endl;
+		}
+	}
 
 private:
-    // -------------------------------------------------------------------------
-    // AUXILARY METHODS
-    // -------------------------------------------------------------------------
-    size_t& updateSize()
-    {
-        return m_size;
-    }
+	// -------------------------------------------------------------------------
+	// AUXILARY METHODS
+	// -------------------------------------------------------------------------
+	size_t& updateSize()
+	{
+		return m_size;
+	}
 
-    // -------------------------------------------------------------------------
-    // DATA
-    // -------------------------------------------------------------------------
-    std::size_t m_maxSize;
-    std::size_t m_size;
-    int m_front;
-    int m_rear;
-    T *m_queue;
+	// -------------------------------------------------------------------------
+	// DATA
+	// -------------------------------------------------------------------------
+	std::size_t m_maxSize;
+	std::size_t m_size;
+	int m_front;
+	int m_rear;
+	T* m_queue;
 };
 
 }  // namespace fifo
--- src/UnorderedList/node.h
+++ src/UnorderedList/node.h
@@ -8,22 +8,25 @@
 namespace dsc {
 namespace lists {
 
-template <typename T>
-struct Node{
-    Node(T data) : data(data), next(nullptr) {}
-    //~Node() { std::cout << "Destroy node with data: " << data << '\n'; }
+template<typename T>
+struct Node {
+	Node(T data)
+		: data(data)
+		, next(nullptr)
+	{
+	}
 
-    T data;
-    std::shared_ptr<Node<T>> next;
+	T data;
+	std::shared_ptr<Node<T>> next;
 };
 
-template <typename T>
+template<typename T>
 Node<T> node(T data)
 {
-    return Node<T>(data);
+	return Node<T>(data);
 }
 
-}
-}
+}  // namespace lists
+}  // namespace dsc
 
 #endif  // Unordered_Single_List_Node_h
